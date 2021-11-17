4180ef246465be08cb035ce83560b70553cf2be4
Mohammad Shariat <mohamadshariatahmadi@gmail.com>
2021-11-17 21:08:59 +0330

    some changes

--- src/App.js
+++ src/App.js
@@ -6,21 +6,51 @@ class App extends Component {
   state = {
     persons: [
       { firstname: "محمد", lastname: "شریعت" },
+      { firstname: "مهراد", lastname: "شریعت" },
       { firstname: "امیر", lastname: "شریعت" },
       { firstname: "علی", lastname: "شریعت" },
       { firstname: "مصطفی", lastname: "شریعت" },
-      { firstname: "مهراد", lastname: "شریعت" },
     ],
+    showPersons: false,
+  };
+
+  showPersonHandler = () => {
+    this.setState({ showPersons: !this.state.showPersons });
   };
 
   render() {
-    const { persons } = this.state;
+    const { persons, showPersons } = this.state;
+
     const styles = {
       textAlign: "center",
     };
+
+    const buttonStyle = {
+      padding: "1em",
+      fontFamily: "BYekan",
+      backgroundColor: "pink",
+      margin: "2em",
+      borderRadius: "10px",
+    };
+
+    let person = null;
+    let buttonText = "";
+
+    if (showPersons) {
+      person = <Persons persons={persons} />;
+      buttonText = "عدم نمایش";
+    } else {
+      buttonText = "نمایش";
+    }
+
     return (
       <div style={styles}>
-        <Persons persons={persons} />
+        <h1>مدیریت کننده اشخاص</h1>
+        <h4>تعداد اشخاص {persons.length} نفر می باشد</h4>
+        {person}
+        <button onClick={this.showPersonHandler} style={buttonStyle}>
+          {buttonText}
+        </button>
       </div>
     );
   }
